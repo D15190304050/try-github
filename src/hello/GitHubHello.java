2b66edf598e5dd35e59049d9ef0e33d73126a30f
D1519304050 <15190304050@163.com>
2022-08-07 10:50:44 +0800

    Keep coding...

---
+++ src/hello/GitHubHello.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class GitHubHello
+{
+    public static void main(String[] args)
+    {
+        // Version Control System.
+        System.out.println("Hello GitHub.");
+    }
+}
